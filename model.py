8b211d700452c177cb7cb44b7c692279272bb0dd
Sefa <mail@sefakalkan.com>
2022-06-04 22:42:35 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,131 +1,124 @@
 import gc
 from random import randint
-import time
+
 import cv2
-import keras
-from keras.models import load_model
+import numpy as np
 from keras import Sequential
+from keras import backend as K
 from keras.applications.resnet_v2 import ResNet101V2
 from keras.applications.vgg19 import VGG19
 from keras.layers import Flatten, Dense, Conv2D, BatchNormalization, Activation, MaxPooling2D, ZeroPadding2D, Dropout
-from matplotlib import pyplot as plt
+from keras.models import load_model
 from scipy.spatial import distance
 from sklearn.model_selection import train_test_split
-from keras import backend as K
-import numpy as np
 
 width = 224
 height = 224
 epochNumber = 40
 nRowSetter = 2000
 
-whichPhoto = randint(1, ((int(nRowSetter * 0.3) - 1)))
+whichPhoto = randint(1, (int(nRowSetter * 0.3) - 1))
 
 
+def mainModel(csv, img, mode):
+
+    cv2.imshow("template", cv2.imread("Template.png"))
 
 
-
-def mainModel(csv,img,mode):
-
-
-    cv2.imshow("Template", cv2.imread("Template.png"))
-
-    trainX, testX, trainY, testY = train_test_split(img, csv , test_size=0.3, random_state=42)
+    trainX, testX, trainY, testY = train_test_split(img, csv, test_size=0.3, random_state=42)
 
     if mode == 3:
         evScoreVGG, imHolderVGG, imHolderVGGEarly = runKerasVGGModel(trainX, testX, trainY, testY, mode)
         print("VGG        |", evScoreVGG[1])
         cv2.imshow("VGG", imHolderVGG)
-        cv2.waitKey()
+
+
     else:
 
-        evScoreVGG, imHolderVGG,imHolderVGGEarly  = runKerasVGGModel(trainX, testX, trainY, testY, mode)
+        evScoreVGG, imHolderVGG, imHolderVGGEarly = runKerasVGGModel(trainX, testX, trainY, testY, mode)
 
         print("VGG        |", evScoreVGG[1])
         cv2.imshow("VGG", imHolderVGG)
+        cv2.imshow("VGGEarly", imHolderVGGEarly)
         del (evScoreVGG, imHolderVGG)
         gc.collect()
 
-        evScoreAlexNet, imHolderAlexNet,imHolderAlexNetEarly = runKerasAlexNetModel(trainX, testX, trainY, testY, mode)
+        cv2.waitKey()
+
+
+        evScoreAlexNet, imHolderAlexNet, imHolderAlexNetEarly = runKerasAlexNetModel(trainX, testX, trainY, testY, mode)
 
         print("AlexNet    |", evScoreAlexNet[1])
         cv2.imshow("AlexNet", imHolderAlexNet)
-        del(evScoreAlexNet,imHolderAlexNet)
+        cv2.imshow("AlexNetEarly", imHolderAlexNetEarly)
+        del (evScoreAlexNet, imHolderAlexNet)
         gc.collect()
 
-        evScoreOwn, imHolderOwn,imHolderOwnEarly = runOwnModel(trainX, testX, trainY, testY, mode)
+        evScoreOwn, imHolderOwn, imHolderOwnEarly = runOwnModel(trainX, testX, trainY, testY, mode)
 
-        print("ResNet     |", evScoreOwn[1])
-        cv2.imshow("ResNet", imHolderOwn)
-        del(evScoreOwn,imHolderOwn)
+        print("Own     |", evScoreOwn[1])
+        cv2.imshow("Own", imHolderOwn)
+        cv2.imshow("OwnEarly", imHolderOwnEarly)
+        del (evScoreOwn, imHolderOwn)
         gc.collect()
+        
 
-        """evScoreResNet, imHolderResnet,imHolderResnetEarly = runResnetKerasModel(trainX, testX, trainY, testY, mode)
+        evScoreResNet, imHolderResnet,imHolderResnetEarly = runResnetKerasModel(trainX, testX, trainY, testY, mode)
 
-        print("Own Model  |", evScoreResNet[1])
-        cv2.imshow("Own Model", imHolderResnet)
+        print("Resnet  |", evScoreResNet[1])
+        cv2.imshow("Resnet", imHolderResnet)
+        cv2.imshow("ResnetEarly", imHolderResnetEarly)
         del(evScoreResNet,imHolderResnet)
         gc.collect()
-
-        cv2.waitKey()"""
-
-
-
-
-
-
-
-
 
 
 
 
 def createKerasResnetModel():
-
     base_model = ResNet101V2(weights='imagenet',
-                            include_top=False,
-                            input_shape=(width, height,3))
+                             include_top=False,
+                             input_shape=(width, height, 3)
+                             )
 
     base_model.trainable = False
 
     model = Sequential([
         base_model,
 
-        Conv2D(164,kernel_size=(7,7)),
-
-
+        Conv2D(164, kernel_size=(7, 7)),
 
         Flatten(),
         Dense(164, activation="softmax"),
 
     ])
+
     return model
 
+
 def createKerasVGG19Model():
-    base_model = VGG19( weights='imagenet',
-                        include_top=False,
-                        input_shape=(width, height,3),
+    base_model = VGG19(weights='imagenet',
+                       include_top=False,
+                       input_shape=(width, height, 3),
 
                        )
     base_model.trainable = False
 
     model = Sequential([
         base_model,
-        Conv2D(164,(3,3),name="firstBreak",activation='ReLU'),
-        MaxPooling2D(pool_size=(2,2),strides=(2,2)),
-        Conv2D(164, (2, 2), name="secondBreak",activation='ReLU'),
+        Conv2D(164, (3, 3), name="firstBreak", activation='ReLU'),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(164, (2, 2), name="secondBreak", activation='ReLU'),
         Flatten(),
         Dense(164, activation="softmax"),
 
     ])
     return model
 
+
 def createOwnModel():
-
-
     model = Sequential([
 
-        Conv2D(96, (11, 11), input_shape=(width, height, 3),),
+        Conv2D(96, (11, 11), input_shape=(width, height, 3), ),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(2, 2)),
@@ -152,17 +145,9 @@ def createOwnModel():
         BatchNormalization(),
         Activation('relu'),
 
-        Conv2D(164, (4,4)),
+        Conv2D(164, (4, 4)),
         BatchNormalization(),
         Activation('relu'),
-
-
-
-
-
-
-
-
 
         Flatten(),
 
@@ -170,28 +155,26 @@ def createOwnModel():
 
     ])
 
-
-
     return model
 
-def createKerasAlexNetModel():
 
+def createKerasAlexNetModel():
     model = Sequential([
-        #1. layer
-        Conv2D(96, (11, 11), input_shape=(width,height,3),
+        # 1. layer
+        Conv2D(96, (11, 11), input_shape=(width, height, 3),
                padding='same'),
         BatchNormalization(),
         Activation('relu'),
-        MaxPooling2D(pool_size=(2,2)),
+        MaxPooling2D(pool_size=(2, 2)),
 
-        #2. layer
+        # 2. layer
         Conv2D(128, (5, 5), padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(2, 2)),
 
         # 3. layer
-        ZeroPadding2D((1,1)),
+        ZeroPadding2D((1, 1)),
         Conv2D(128, (3, 3), padding='same'),
         BatchNormalization(),
         Activation('relu'),
@@ -203,13 +186,12 @@ def createKerasAlexNetModel():
         BatchNormalization(),
         Activation('relu'),
 
-
         # 5. layer
         ZeroPadding2D((1, 1)),
         Conv2D(128, (3, 3), padding='same'),
         BatchNormalization(),
         Activation('relu'),
-        MaxPooling2D(pool_size=(2, 2)), #21
+        MaxPooling2D(pool_size=(2, 2)),  # 21
 
         # 6. layer
         Flatten(),
@@ -226,7 +208,7 @@ def createKerasAlexNetModel():
 
         # 8. layer
         Dense(164),
-        BatchNormalization(), #32
+        BatchNormalization(),  # 32
         Activation('softmax'),
 
     ])
@@ -234,8 +216,7 @@ def createKerasAlexNetModel():
     return model
 
 
-def runKerasAlexNetModel(trainX,testX,trainY,testY,mode):
-
+def runKerasAlexNetModel(trainX, testX, trainY, testY, mode):
     modelKeras = load_model("pretrained/AlexNet.h5")
     tempArrHolder = []
 
@@ -257,18 +238,16 @@ def runKerasAlexNetModel(trainX,testX,trainY,testY,mode):
 
     imgHolder = makePredictionWithImg(predictedValue, mode, testX, testY)
 
-    return evScore, imgHolder,tempImg
+    return evScore, imgHolder, tempImg
 
 
-def runKerasVGGModel(trainX,testX,trainY,testY,mode):
-
-
+def runKerasVGGModel(trainX, testX, trainY, testY, mode):
     modelKeras = load_model("pretrained/VGG.h5")
 
-    tempArrHolder= []
+    modelKeras.summary()
+    tempArrHolder = []
 
-
-    tempOut = [  modelKeras.layers[3].output]
+    tempOut = [modelKeras.layers[3].output]
 
     functors = [K.function([modelKeras.input], [out]) for out in tempOut]
 
@@ -276,7 +255,7 @@ def runKerasVGGModel(trainX,testX,trainY,testY,mode):
         x = x[np.newaxis, ...]
         tempArrHolder.append(functors[0](x))
 
-    tempArrHolder = np.reshape(tempArrHolder,newshape=(-1,164))
+    tempArrHolder = np.reshape(tempArrHolder, newshape=(-1, 164))
 
     tempImg = makePredictionWithImg(tempArrHolder, mode, testX, testY)
 
@@ -286,28 +265,13 @@ def runKerasVGGModel(trainX,testX,trainY,testY,mode):
 
     imgHolder = makePredictionWithImg(predictedValue, mode, testX, testY)
 
+    return evScore, imgHolder, tempImg
 
 
-    cv2.imshow("Early",tempImg)
-
-
-
-
-
-
-
-
-
-    return evScore, imgHolder,tempImg
-
-
-def runOwnModel(trainX,testX,trainY,testY,mode):
-
-    tempArrHolder= []
+def runOwnModel(trainX, testX, trainY, testY, mode):
+    tempArrHolder = []
 
     modelKeras = load_model("pretrained/Own.h5")
-
-
 
     tempOut = [modelKeras.layers[22].output]
 
@@ -317,7 +281,7 @@ def runOwnModel(trainX,testX,trainY,testY,mode):
         x = x[np.newaxis, ...]
         tempArrHolder.append(functors[0](x))
 
-    tempArrHolder = np.reshape(tempArrHolder,newshape=(-1,164))
+    tempArrHolder = np.reshape(tempArrHolder, newshape=(-1, 164))
 
     tempImg = makePredictionWithImg(tempArrHolder, mode, testX, testY)
 
@@ -327,21 +291,13 @@ def runOwnModel(trainX,testX,trainY,testY,mode):
 
     imgHolder = makePredictionWithImg(predictedValue, mode, testX, testY)
 
-    return evScore, imgHolder,tempImg
+    return evScore, imgHolder, tempImg
 
 
-def runResnetKerasModel(trainX,testX,trainY,testY,mode):
-
-    modelKeras = createKerasResnetModel()
-
-    modelKeras.summary()
+def runResnetKerasModel(trainX, testX, trainY, testY, mode):
+    modelKeras = load_model("pretrained/ResNet.h5")
 
     tempArrHolder = []
-
-    modelKeras.compile(loss='sparse_categorical_crossentropy', optimizer="ADAM",
-                       metrics=['sparse_categorical_accuracy'])
-
-    modelKeras.fit(trainX, trainY, epochs=epochNumber, validation_data=(testX, testY))
 
     tempOut = [modelKeras.layers[2].output]
 
@@ -357,30 +313,23 @@ def runResnetKerasModel(trainX,testX,trainY,testY,mode):
 
     evScore = modelKeras.evaluate(testX, testY)
 
-    predictedValue = modelKeras.predict(testX,mode)
+    predictedValue = modelKeras.predict(testX, mode)
 
-    imgHolder = makePredictionWithImg(predictedValue,mode,testX,testY)
+    imgHolder = makePredictionWithImg(predictedValue, mode, testX, testY)
 
-    modelKeras.save("pretrained/ResNet.h5")
-
-    return evScore,imgHolder,tempImg
+    return evScore, imgHolder, tempImg
 
 
-def makePredictionWithImg(predictedValue,mode,testX,testY):
+def makePredictionWithImg(predictedValue, mode, testX, testY):
     simPic = [distance.cosine(predictedValue[whichPhoto], img) for img in predictedValue]
-
-
 
     print("Index of the photo : ", whichPhoto)
 
     kipkop = sorted(range(len(simPic)), key=lambda k: simPic[k])
-    if(mode == 2):
+    if (mode == 2):
         bipbop = createBipBop(kipkop, testY)
     else:
         bipbop = sorted(range(len(simPic)), key=lambda k: simPic[k])[1:6]
-
-
-
 
     # add if bipbop[n] == 0 print noFound.jpg
 
@@ -391,13 +340,12 @@ def makePredictionWithImg(predictedValue,mode,testX,testY):
     sumCon = cv2.vconcat([firstCon, secCon])
     sumCon2 = cv2.vconcat([sumCon, thrdCon])
 
-
-
     return sumCon2
+
 
 def createBipBop(fullbop, testY):  # name veriables like normal human beign at some point
 
-    tempCounter = 0;
+    tempCounter = 0
 
     bipbop = [0, 0, 0, 0, 0]
 
@@ -410,9 +358,10 @@ def createBipBop(fullbop, testY):  # name veriables like normal human beign at s
 
                 tempCounter += 1
         if (tempCounter == 4):
-            break;
+            break
 
     return bipbop
+
 
 def belongedClass(temp, testY, bipbop):
     valueToReturn = -1
@@ -424,7 +373,8 @@ def belongedClass(temp, testY, bipbop):
 
     accesories = [24, 25, 26, 27, 28, 29, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 51, 73, 74, 75, 84, 103, 112, 113,
                   114, 115, 116, 117, 118, 122, 123, 124, 128, 137, 138, 139, 142, 143, 144, 154, 155, 157, 158, 159]
-    top = [0, 9, 11, 12, 13, 14, 48, 60, 62, 63, 67, 72, 85, 86, 87, 88, 99, 120, 125, 130, 133, 147, 148, 149, 150, 152,
+    top = [0, 9, 11, 12, 13, 14, 48, 60, 62, 63, 67, 72, 85, 86, 87, 88, 99, 120, 125, 130, 133, 147, 148, 149, 150,
+           152,
            163]
     sumOfFullLayer = [0, 1, 2, 3, 10, 15, 16, 17, 21, 22, 23, 52, 58, 59, 64, 65, 71, 89, 90, 94, 95, 97, 98, 102, 126,
                       127, 131, 132, 145, 146, 151, 160, 161]
@@ -453,5 +403,3 @@ def belongedClass(temp, testY, bipbop):
         print("You forgot to implement : ", temp)
 
     return valueToReturn
-
-
